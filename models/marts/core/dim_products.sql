5195859765a69b5d6e401a0cc4cb9cbc44b87336
aditya-v1 <100793732+aditya-v1@users.noreply.github.com>
2022-04-13 23:48:11 +0530

    Added dim_products model and its yml file (#16)
    * Added dim_products model and its yml file
    
    * Made changes as per review comments, Added total_unique_buyers, made formatting changes. changed field name from total_products_sales to  total_product_revenue
    
    * fixed typo in dim_products.yml file
    
    * Made formatting changes.  Added a line between fields and from clause for better readabiliity

---
+++ models/marts/core/dim_products.sql
@@ -0,0 +1,57 @@
+with stg_products as (
+    select * from  {{ref("stg_production__products")}}
+),
+
+stg_order_items as (
+    select * from {{ref("stg_transactions__order_items")}}
+),
+
+agg_order_items as (
+    select 
+        product_id,
+        sum(quantity) as total_units_sold,
+        sum(subtotal) as total_product_revenue,
+        min(order_created_at) as product_first_order_at,
+        max(order_created_at) as product_recent_order_at,
+        count(distinct customer_id) as total_unique_buyers
+
+    from stg_order_items
+    group by product_id
+),
+
+final as (
+    select 
+        -- primary key
+        stg_products.product_id,
+        
+        --direct fields
+        stg_products.product_name,
+        stg_products.product_cost,
+        stg_products.product_description,
+        stg_products.product_size,
+        stg_products.product_type,
+
+        --derived fields
+        coalesce(agg_order_items.total_unique_buyers,0) as total_unique_buyers,
+        coalesce(agg_order_items.total_units_sold,0) as total_units_sold,
+        coalesce(agg_order_items.total_product_revenue,0) as total_product_revenue,
+        agg_order_items.product_first_order_at,
+        agg_order_items.product_recent_order_at,
+        rank() over(
+            order by 
+                agg_order_items.total_units_sold 
+                desc) 
+        as total_units_sold_rank,
+        rank() over(
+            order by 
+                agg_order_items.total_product_revenue 
+                desc) 
+        as total_product_revenue_rank
+
+    from stg_products 
+    left join agg_order_items 
+        on stg_products.product_id = agg_order_items.product_id
+    
+)
+
+select * from final 
