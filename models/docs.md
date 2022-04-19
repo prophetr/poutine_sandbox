514767eadb1f18c012ae4f650b6c1ed3a19ce161
Maher A <82591366+ayammaher@users.noreply.github.com>
2022-04-19 18:44:02 -0400

    feat: implement fct_trans_order_items (#24hhtjb) (#17)
    * Added folder and file for trans_order_items
    
    * WIP
    
    * Added orders_source to model
    
    * Implemented changes
    
    * Implemented PR changes
    
    * Changes implemented
    
    * Implemented changes
    
    * Update fct_orders.yml
    
    Add newline
    
    Co-authored-by: Martin Guindon <14781590+MartinGuindon@users.noreply.github.com>

---
+++ models/docs.md
@@ -0,0 +1,43 @@
+{% docs fct_orders__orders_sk %}
+Unique identifier for order_items table.
+{% enddocs %}
+
+{% docs fct_orders__order_item_unique_sk %}
+Unique identifier for orders.
+{% enddocs %}
+
+{% docs fct_orders__order_id %}
+Unique order identifier.
+{% enddocs %}
+
+{% docs fct_orders__product_id %}
+Unique product identifier.
+{% enddocs %}
+
+{% docs fct_orders__customer_id %}
+Unique customer identifier.
+{% enddocs %}
+
+{% docs fct_orders__order_quantity %}
+The number of items ordered by the customer.
+{% enddocs %}
+
+{% docs fct_orders__product_cost %}
+The cost of the product
+{% enddocs %}
+
+{% docs fct_orders__subtotal %}
+The order's subtotal
+{% enddocs %}
+
+{% docs fct_orders__order_created_at %}
+When the order was created at.
+{% enddocs %}
+
+{% docs fct_orders__order_tax %}
+The tax on the order
+{% enddocs %}
+
+{% docs fct_orders__order_total %}
+The order's total amount
+{% enddocs %}
---
+++ models/marts/transactions/fct_orders.sql
@@ -0,0 +1,51 @@
+with order_items as (
+
+  select * from {{ ref('stg_transactions__order_items') }}
+
+), 
+
+customers as (
+
+  select * from {{ ref('stg_production__customers') }}
+
+), 
+
+orders as (
+
+  select * from {{ ref('stg_transactions__orders') }} 
+
+), 
+
+final as  (
+
+    select
+        -- surrogate key
+        {{ dbt_utils.surrogate_key(['order_items.order_item_unique_sk',
+            'order_items.order_id']) 
+        }} as orders_sk,
+
+        --foreign key
+        order_items.order_item_unique_sk,
+        order_items.order_id,
+        order_items.product_id,
+        order_items.customer_id,
+
+        --facts
+        order_items.quantity as order_quantity,
+        order_items.product_cost,
+        order_items.subtotal,
+        orders.order_tax,
+        orders.order_total,
+
+        -- timestamps
+        order_items.order_created_at
+
+    from order_items
+        left join customers 
+            on order_items.customer_id = customers.customer_id
+        left join orders
+            on order_items.order_id = orders.order_id
+
+)
+
+select * from final
