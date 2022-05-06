1dab9805ae9371b45cc866e914cc006143e94d81
wyandata <97130935+wyandata@users.noreply.github.com>
2022-05-06 15:35:35 -0400

    Created fct_fulfillments sql and yml

---
+++ models/marts/core/fct_fulfillments.sql
@@ -0,0 +1,39 @@
+with fulfillments as (
+    select * from {{ ref('stg_production_fulfillments') }}
+),
+
+customers as (
+    select * from {{ ref('stg_production__customers') }}
+),
+
+orders as (
+    select * from {{ ref('stg_transactions__orders') }}
+),
+
+final as (
+    select
+        -- primary key
+        event_id as fulfillment_event_id,
+
+        -- foreign keys
+        order_id,
+        customer_id,
+
+        -- dates & timestamps
+        created_at,
+
+        -- details
+        event_name
+
+        -- metrics: TODO would be intereting to have a most_recent_order snapshot
+
+    from fulfillments
+    left join customers
+        on fulfillments.customer_id = customers.customer_id
+    left join orders
+        on fulfillments.order_id = orders.order_id
+)
+
+select * from final
+
+
